da05225f3ca8dd1c2aa8c75fde53823e839556d1
lijinsheng <lijsd@yonyou.com>
2023-05-19 16:26:41 +0800

    init

--- src/gortc.go
+++ src/gortc.go
@@ -8,15 +8,60 @@ import (
 	"github.com/sirupsen/logrus"
 	"gortc/src/config"
 	"gortc/src/util"
+	"strconv"
 )
 
-func main() {
-	util.LogConfig()
+var pc1 *webrtc.PeerConnection
 
+func chat() {
+	// pc1 创建一个 data channel
+	dc1, _ := pc1.CreateDataChannel("data", nil)
+
+	// 设置 dc1 的消息处理函数
+	dc1.OnMessage(func(msg webrtc.DataChannelMessage) {
+		fmt.Println("Message from dc1: " + string(msg.Data))
+	})
+	// 发送消息给 dc2
+	dc1.SendText("Hello from dc1")
+
+}
+func iceChange() {
+	fmt.Println("input other sdb:")
+	remoteSdp := webrtc.SessionDescription{}
+	var remoteSdpStr string
+	n, err := fmt.Scanln(&remoteSdpStr)
+	if err != nil {
+		panic(err)
+	}
+	logrus.Debug("读取" + strconv.Itoa(n) + "个字节,远程sdp是:" + remoteSdpStr)
+	remoteSdpBuffer, err := base64.StdEncoding.DecodeString(remoteSdpStr)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(remoteSdpBuffer, &remoteSdp)
+	if err != nil {
+		panic(err)
+	}
+	err = pc1.SetRemoteDescription(remoteSdp)
+	if err != nil {
+		panic(err)
+	}
+	chat()
+}
+
+func onICECandidate(cdt *webrtc.ICECandidate) {
+	logrus.Debug(cdt.ToJSON())
+	sdp := pc1.LocalDescription()
+	bytes, _ := json.Marshal(sdp)
+	fmt.Println(string(bytes))
+	sdpBase64 := base64.StdEncoding.EncodeToString(bytes)
+	logrus.Info("local sdp:" + sdpBase64)
+	iceChange()
+}
+
+func initConnection() {
 	appConfig, _ := config.GetConfig()
 	app := appConfig.App
-	fmt.Println("hello")
-	logrus.Debug("ok....")
 	// 创建媒体引擎和 API 对象
 	m := webrtc.MediaEngine{}
 	err := m.RegisterDefaultCodecs()
@@ -34,72 +79,34 @@ func main() {
 			CredentialType: webrtc.ICECredentialTypePassword,
 		},
 	}
+	for _, server := range iceServers {
+		bytes, _ := server.MarshalJSON()
+		logrus.Debug("ICE Server:" + string(bytes))
+	}
 
 	cfg := webrtc.Configuration{
 		ICEServers: iceServers,
 	}
 
 	// 创建两个 PeerConnection 对象
-	pc1, err := api.NewPeerConnection(cfg)
+	pc1, err = api.NewPeerConnection(cfg)
 	if err != nil {
 		logrus.Error(err)
 		return
 	}
-	pc1Sdp := pc1.LocalDescription()
-	pc1sdpBuffer, err := json.Marshal(pc1Sdp)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-	logrus.Debug(pc1sdpBuffer)
-
-	pc1sdpStr := base64.StdEncoding.EncodeToString(pc1sdpBuffer)
-	logrus.Debug("SDP信息:" + pc1sdpStr)
-
-	remoteSdp := webrtc.SessionDescription{}
-	remoteSdpStr := "xxxx"
-	remoteSdpBuffer, err := base64.StdEncoding.DecodeString(remoteSdpStr)
+	offer, err := pc1.CreateOffer(nil)
 	if err != nil {
 		panic(err)
 	}
-	err = json.Unmarshal(remoteSdpBuffer, &remoteSdp)
-	if err != nil {
-		panic(err)
-	}
-	err = pc1.SetRemoteDescription(remoteSdp)
-	if err != nil {
-		panic(err)
-	}
-	answer, err := pc1.CreateAnswer(nil)
-	if err != nil {
-		panic(err)
-	}
-	err = pc1.SetLocalDescription(answer)
-	if err != nil {
-		panic(err)
-	}
-
-	// pc1 创建一个 data channel
-	dc1, _ := pc1.CreateDataChannel("data", nil)
-
-	// 设置 dc1 的消息处理函数
-	dc1.OnMessage(func(msg webrtc.DataChannelMessage) {
-		fmt.Println("Message from dc1: " + string(msg.Data))
-	})
-
-	// pc1 创建 offer
-	offer, _ := pc1.CreateOffer(nil)
-
-	// pc1 设置本地描述
 	err = pc1.SetLocalDescription(offer)
 	if err != nil {
-		logrus.Error(err)
-		return
+		panic(err)
 	}
-
-	// 发送消息给 dc2
-	dc1.SendText("Hello from dc1")
-
+	pc1.OnICECandidate(onICECandidate)
+}
+func main() {
+	util.LogConfig()
+	initConnection()
 	// 等待，让数据有时间发送
 	select {}
 }
